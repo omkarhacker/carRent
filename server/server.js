9675d43a029a24246c90c085dc1126c9ff6d1070
Omkar Gadam <omkargangadhargadam@gmail.com>
2024-11-18 03:05:13 +0530

    changed server file

--- server/server.js
+++ server/server.js
@@ -20,18 +20,16 @@ app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 const path = require('path');
 const { SystemModule } = require('@faker-js/faker');
 
-if(process.env.NODE_ENV==='production')
-{
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use('/', express.static('client/build'));
 
-    app.use('/' , express.static('client/build'))
-
-    app.get('*' , (req , res)=>{
-
-          res.send('Hello world')//File(path.resolve(__dirname, 'client/build/index.html'));
-
-    })
-
+  app.get('*', (req, res) => {
+      // Serve index.html for all other routes
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
