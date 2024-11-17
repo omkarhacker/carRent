ed458afe0024c13bb1a4e579a14c9df93a221de1
Omkar Gadam <omkargangadhargadam@gmail.com>
2024-11-18 02:59:01 +0530

    changes router

--- client/src/App.js
+++ client/src/App.js
@@ -1,87 +1,86 @@
 import './App.css';
-import { Navigate } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { useEffect } from 'react'; 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import BookingCar from './pages/BookingCar';
+import {Route , BrowserRouter , Navigate} from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import BookingCar from './pages/BookingCar'
 import UserBookings from './pages/UserBookings';
 import AddCar from './pages/AddCar';
 import AdminHome from './pages/AdminHome';
 import EditCar from './pages/EditCar';
 
-export const ProtectedRoute = ({ element }) => {
+export const ProtectedRoute = ({ children }) => {
+  
   if (localStorage.getItem('user')) {
     // user is authenticated
-    return element;
+    return children;
   }
   return <Navigate to="/login" />;
 };
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <ProtectedRoute element={<Home />} />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
-  },
-  {
-    path: '/booking/:carid',
-    element: <ProtectedRoute element={<BookingCar />} />,
-    loader: ({ params }) => {
+    path: "/",
+    element: <ProtectedRoute><Home/></ProtectedRoute>,
+  },{
+    path: "/login",
+    element: <Login/>
+  },{
+    path: "/register",
+    element: <Register/>
+  },{
+    path: "/booking/:carid",
+    element: <ProtectedRoute><BookingCar /></ProtectedRoute>,
+    loader: ({params})=>{
       return params.carid;
-    },
-  },
-  {
-    path: '/userbookings',
-    element: <ProtectedRoute element={<UserBookings />} />,
-  },
-  {
-    path: '/addcar',
-    element: <ProtectedRoute element={<AddCar />} />,
-  },
-  {
-    path: '/editcar/:carid',
-    element: <ProtectedRoute element={<EditCar />} />,
-    loader: ({ params }) => {
+    }
+  },{
+    path: "userbookings",
+    element: <ProtectedRoute><UserBookings/></ProtectedRoute>
+  },{
+    path: "addcar",
+    element: <ProtectedRoute><AddCar/></ProtectedRoute>
+  },{
+    path: "editcar/:carid",
+    element: <ProtectedRoute><EditCar/></ProtectedRoute>,
+    loader: ({params})=>{
       return params.carid;
-    },
-  },
-  {
-    path: '/admin',
-    element: <ProtectedRoute element={<AdminHome />} />,
-  },
+    }
+  },{
+    path: "admin",
+    element: <ProtectedRoute><AdminHome/></ProtectedRoute>
+  }
 ]);
 
 function App() {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleResizeObserverError = (e) => {
-        if (e.message === 'ResizeObserver loop completed with undelivered notifications.') {
-          e.stopImmediatePropagation();
-        }
-      };
-
-      window.addEventListener('error', handleResizeObserverError);
-
-      return () => {
-        window.removeEventListener('error', handleResizeObserverError); // Cleanup on unmount
-      };
-    }
-  }, []); // Empty dependency array ensures this runs once after the component mounts
-
   return (
     <div className="App">
-      <RouterProvider router={router} />
+<RouterProvider router={router}/>
+         
+         
+         {/* <BrowserRouter>
+         
+             <ProtectedRoute path='/' exact component={Home} />
+             <Route path='/login' exact component={Login} />
+             <Route path='/register' exact component={Register} />
+             <ProtectedRoute path='/booking/:carid' exact component={BookingCar} />
+             <ProtectedRoute path='/userbookings' exact component={UserBookings} />
+             <ProtectedRoute path='/addcar' exact component={AddCar} />
+             <ProtectedRoute path='/editcar/:carid' exact component={EditCar} />
+             <ProtectedRoute path='/admin' exact component={AdminHome} />
+         
+         </BrowserRouter> */}
+
     </div>
   );
 }
 
+
+
 export default App;
+
+
